ac59ca332903ffcc5365ac5b8a1309eb0e130cec
kafeikelele <1046641572@qq.com>
2016-10-06 15:03:27 +0800

    modify index

--- alex/forth/reflect/index.py
+++ alex/forth/reflect/index.py
@@ -4,7 +4,16 @@ Created on 2016年10月6日
 
 @author: wxh
 '''
+'''
+from backend import account
+#account.login()
+data = raw_input("请输入地址: ")
+if data == 'account/login':
+    account.login()
+elif data == 'account/logout':
+    account.logout()
 
+'''
 
 data = raw_input('请输入网址: ')
 array = data.split('/')
@@ -14,7 +23,4 @@ model =getattr(userspance, array[0])
 func = getattr(model,array[1])
 func()
 
-'''
-from backend import account
-account.login()
-'''
+
